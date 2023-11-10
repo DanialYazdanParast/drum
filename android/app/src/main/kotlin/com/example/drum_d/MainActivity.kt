a425fce8ea7c81404418be8cfa00d6deaa02ab15
Danial YazdanParast <150327854+DanialYazdanParast@users.noreply.github.com>
2023-11-10 04:43:51 +0330

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/drum_d/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.drum_d
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,114 @@
+import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
+
+void main() {
+  runApp(const Appication());
+}
+
+class Appication extends StatelessWidget {
+  const Appication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return getAppication();
+  }
+}
+
+Widget getAppication() {
+  return MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+      backgroundColor: Colors.blue,
+      body: SafeArea(
+          child: DecoratedBox(
+        decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage('images/drum.png'), fit: BoxFit.fitWidth),
+        ),
+        position: DecorationPosition.background,
+        child: getBody(),
+      )),
+    ),
+  );
+}
+
+Widget getBody() {
+  return Column(
+    children: [
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound('audio/h1.wav');
+                },
+                child: Text(''),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound('audio/h2.wav');
+                },
+                child: Text(''),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound('audio/c1.wav');
+                },
+                child: Text(''),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound('audio/c2.wav');
+                },
+                child: Text(''),
+              ),
+            ),
+          ],
+        ),
+      ),
+      Expanded(
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound('audio/k1.wav');
+                },
+                child: Text(''),
+              ),
+            ),
+            Expanded(
+              child: TextButton(
+                onPressed: () {
+                  playSound('audio/k2.wav');
+                },
+                child: Text('aaa'),
+              ),
+            ),
+          ],
+        ),
+      ),
+    ],
+  );
+}
+
+playSound(String sound) {
+  var player = AudioPlayer();
+  player.play(AssetSource(sound));
+}
